9ee148a37f6155d6872a60b9bd26f790efd5b4fb
gmahima <mahimagangavarapu@gmail.com>
2020-11-16 19:56:30 +0530

    scroll problem

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,205 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { Component, createRef, useRef, useState , useEffect} from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const checkScrollSpeed = (function(settings) {
+    settings = settings || {};
+  
+    let lastPos, newPos, timer, delta,
+        delay = settings.delay || 50;
+  
+    function clear() {
+      lastPos = null;
+      delta = 0;
+    }
+  
+    clear();
+  
+    return function() {
+      newPos = window.scrollY;
+      if (lastPos != null) { // && newPos < maxScroll
+        delta = newPos - lastPos;
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+  })();
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  function useCrazyScroll(){
+      const sectionsRef = useRef()
+      const [programmaticScrollTo, setProgrammaticScrollTo] = useState(null)
+      const lastScrollTop = useRef(0);
+      
+      function userScrollHandler(e) {
+        console.log("lel", e.isTrusted)
+        var st = window.pageYOffset || document.documentElement.scrollTop
+        var speed = checkScrollSpeed();
+        console.log({speed})
+        const jump = Math.abs(window.scrollY -  lastScrollTop.current );
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        console.log({jump})
+        if(st > lastScrollTop.current){
+            speed = Math.abs(speed)
+            // downscroll
+            if ( st < sectionsRef.current.offsetTop && speed < 100) {
+                console.log("SCROLL");
+                setProgrammaticScrollTo(sectionsRef.current.offsetTop)                
+            } 
+        }
+        lastScrollTop.current = st <=0  ? 0 : st;
+    }
+    const userScrollHandlerRef = useRef(userScrollHandler)
+    useEffect(() => {
+        userScrollHandlerRef.current = userScrollHandler
+    })
+    function programmaticScrollHandler(e) {
+       if(window.scrollY === programmaticScrollTo){
+           setProgrammaticScrollTo(null)
+       }
+    }
+      useEffect(() => {
+          if(programmaticScrollTo !== null){
+            window.addEventListener('scroll', programmaticScrollHandler);
+            window.scrollTo({top: programmaticScrollTo, behavior: 'smooth'})
+            return () => {
+                window.removeEventListener('scroll', programmaticScrollHandler);        
+            }              
+          }else{
+            function handle(e){
+                userScrollHandlerRef.current(e)
+            }
+            window.addEventListener('scroll', handle);
+            return () => {
+                window.removeEventListener('scroll', handle);        
+            }
+          }
+        
+      }, [programmaticScrollTo])
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      function goToTop(){
+          setProgrammaticScrollTo(0);
+      }
+      return [sectionsRef, goToTop]
+  }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+function MyComp(props) {
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    const [sectionsRef, goToTop] = useCrazyScroll()
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    // constructor() {
+    //     super();
+    //     this.scrollToSections = this.scrollToSections.bind(this);
+    //     this.sectionsRef = createRef()
+       
+    // }
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+    // componentDidMount() {
+    //     this.lastScrollTop = 0
+    //     window.addEventListener('scroll', this.scrollToSections, false);
+    // }
+
+    // componentWillUnmount() {
+    //     window.removeEventListener('scroll', this.scrollToSections, false);
+    // }
+
+    // scrollToSections(e) {
+    //     console.log("lel", e.isTrusted)
+    //     var st = window.pageYOffset || document.documentElement.scrollTop
+    //     var speed = checkScrollSpeed();
+        
+    //     if(st > this.lastScrollTop){
+    //         speed = Math.abs(speed)
+    //         // downscroll
+    //         if ( st < this.sectionsRef.current.offsetTop && speed < 100) {
+    //             console.log("SCROLL");
+    //             window.scrollTo({top: this.sectionsRef.current.offsetTop, behavior: 'smooth'})
+    //         } 
+    //     }
+    //     this.lastScrollTop = st <=0  ? 0 : st;
+
+    // }
+
+    
+
+        let partitionData = {
+          sections: [1, 2, 3, 4]
+        };
+        let sections = [];
+
+        if (partitionData.sections) {
+            partitionData.sections.forEach((section, i) => {
+                sections.push(
+                    <div className="partition-section">
+                        asdf
+                        <hr />
+                    </div>
+                );
+            });
+        }
+
+
+        return (
+            <div className="partition" >
+                <div className="partition-1">
+                    <div className={"partition-headline partition-headline-left"}>
+                        <h5 data-swiper-parallax="-600">title</h5>
+                        description
+                    </div>
+                    <div className="partition-gradient" />
+                </div>
+                <div ref={sectionsRef}>
+                    <hwr/>
+                    <div className="partition-sections">
+                        {sections}
+                    </div>
+                </div>
+
+                <div className="partition-footer">
+                    <h2>data 2</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 3</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 4</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 5</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 5</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 5</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+                <div className="partition-footer">
+                    <h2>data 5</h2>
+                    <div className="partition-footer-back-top">
+                        data
+                    </div>
+                </div>
+            </div>
+        )
+        }
+
+
+export default MyComp
+
